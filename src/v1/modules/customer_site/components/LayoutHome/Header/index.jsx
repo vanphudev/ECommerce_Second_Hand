e4dbfd1ea292dac4cec39ca867a5280945642062
tranhau2607 <tranhau2607@gmail.com>
2024-10-03 18:31:15 +0700

    Mới nhất #1

--- src/v1/modules/customer_site/components/LayoutHome/Header/index.jsx
+++ src/v1/modules/customer_site/components/LayoutHome/Header/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 const Header = () => {
    return (
       <>
-         <h1>Trang Header</h1>
+         <h1 >Trang Header</h1>
+         <h3 ><Link to={import.meta.env.VITE_ROUTER_MODULES_CUSTOMER_SITE_LANDING_DEFAULT}>Landing</Link></h3>
       </>
    );
 };
